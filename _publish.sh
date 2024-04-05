835e57616692a500afac07e6b2cf6325d67a2a9e
JulienGAMartin <julien.martin@uottawa.ca>
2024-04-05 00:45:29 -0400

    update pages

--- _publish.sh
+++ _publish.sh
@@ -1,8 +1,20 @@
-git checkout gh-pages
-#git checkout main .gitignore
-git rm -r .
-cp -r _book/. .
-git add .
-git commit -a -m "update pages"
-git push
-git checkout main
+#!/usr/bin/env bash
+
+read -p "Have you updated the language links with fix_links? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo " you are ready to update to gh-pages"
+  git checkout gh-pages
+  #git checkout main .gitignore
+  git rm -r .
+  cp -r _book/. .
+  git add .
+  git commit -a -m "update pages"
+  git push
+  git checkout main
+  echo "done"
+else
+  echo "run _fix-links.r then run _publish.sh again"
+fi
+
