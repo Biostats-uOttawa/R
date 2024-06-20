81440213e0de7ba77b4474fd62adf72cf43ec2f8
JulienGAMartin <julien.martin@uottawa.ca>
2024-06-20 16:11:54 -0400

    better render workflow

--- _render-all.sh
+++ _render-all.sh
@@ -2,12 +2,16 @@
 
 lang=("en" "fr") 
 
+cdir=`pwd`
+
 for i in "${lang[@]}"; do
+  echo Purging files in _book/$i
   echo Rendering $i
+    rm -R $cdir/_book/$i
   export QUARTO_PROFILE=$i
   cd lang/$i
   quarto render --profile $i
-  
+  cd $cdir
 done
 
   echo Done rendering now fixing links
